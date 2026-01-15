4fd7c8baecd9ab5a16332aada25b1323db3301d2
deepsandilya01 <deepsandilya23@gmail.com>
2026-01-15 23:07:18 +0530

    upadate1.0

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from "react";
 
 const TaskList = () => {
   return (
-    <div className='h-[55%] w-full bg-red-500 mt-10'>
-      
+    <div
+      id="taskList"
+      className="h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10"
+    >
+      <div className="shrink-0 h-full w-75 p-5 bg-red-400 rounded-xl ">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">15 jun 2026</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Complete this project</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          ad doloribus eum magnam vero? Voluptatum nihil dignissimos quo.
+        </p>
+      </div>
+      <div className="shrink-0 h-full w-75 p-5 bg-yellow-400 rounded-xl ">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">15 jun 2026</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Complete this project</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          ad doloribus eum magnam vero? Voluptatum nihil dignissimos quo.
+        </p>
+      </div>
+      <div className="shrink-0 h-full w-75 p-5 bg-green-400 rounded-xl ">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">15 jun 2026</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Complete this project</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          ad doloribus eum magnam vero? Voluptatum nihil dignissimos quo.
+        </p>
+      </div>
+      <div className="shrink-0 h-full w-75 p-5 bg-blue-400 rounded-xl ">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">15 jun 2026</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Complete this project</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          ad doloribus eum magnam vero? Voluptatum nihil dignissimos quo.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
